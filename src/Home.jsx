0d30fac0a022807436a623c191323d3f0f693695
killshotxd <hassanansari211@gmail.com>
2022-10-20 10:52:31 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import styles from "./Home.module.css";
 
@@ -9,10 +9,28 @@ const URL_OFFICE = "https://excuser.herokuapp.com/v1/excuse/office";
 const URL_FAMILY = "https://excuser.herokuapp.com/v1/excuse/family";
 
 const Home = () => {
+  // Functions
+  const [officePost, setOfficePost] = useState("");
+  const [familyPost, setFamilyPost] = useState("");
+  const office = () => {
+    axios.get(URL_OFFICE).then((response) => {
+      setOfficePost(response.data[0].excuse);
+    });
+  };
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.header}>
         <h2>Generate Random Excuses 😝</h2>
+
+        <div className={styles.excuses}>
+          <button onClick={office}>Excuse for Office 🙄</button>
+          <p>{officePost}</p>
+        </div>
+
+        <div className={styles.excuses2}>
+          <button onClick={office}>Excuse for Family 🙄</button>
+          <p>{familyPost}</p>
+        </div>
       </div>
     </div>
   );
